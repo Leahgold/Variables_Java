6e0cc895c7ed00fccda71f921d86d55e3bd4c760
Leah Gold <ira.bearn@gmail.com>
2024-01-15 15:37:09 +0200

    про разные переменные

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] massiv) {
+        byte b = 15;
+        System.out.println("b=" + b);
+        short s = 30123;
+        System.out.println("s=" + s);
+        int i = 8;
+        System.out.println("i=" + i);
+        long l = 13;
+        System.out.println("l=" + l);
+        double d = 1.5;
+        System.out.println("d=" + d);
+        String st = "String";
+        System.out.println("st=" + st);
+        char c = 'T';
+        System.out.println("c=" + c);
+
+
+
+    }
+}
